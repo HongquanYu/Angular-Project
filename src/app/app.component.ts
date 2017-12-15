a719bfb9e23e979190661030454899dc622c93f6
Hongquan Yu <hyu12346@terpmail.umd.edu>
2017-12-14 19:47:39 -0500

    Added authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBaSi62OM7sh05Bhxoo26NvSOwewUdUNkI",
+      authDomain: "recipe-a50c7.firebaseapp.com",
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
